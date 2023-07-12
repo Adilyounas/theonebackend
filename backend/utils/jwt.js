66b2b272f6dcaa9d8ec9d40213ef674597343192
Muhammad Adil <addhtml00@gmail.com>
2023-07-12 10:10:47 +0500

    secure flag added in jwt.js

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -12,6 +12,7 @@ const sendJWT = async (user, res, statusCode, message) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: true,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
