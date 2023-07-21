796e4ff3a1c924bd132e173a6f0fd519564efd3a
Muhammad Adil <addhtml00@gmail.com>
2023-07-21 09:50:04 +0500

    log added auth.js

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -6,6 +6,7 @@ const User = require("../models/userModel")
 const isAuthenticated = async (req, res, next) => {
   try {
     const { token } =  req.cookies;
+    console.log(token);
     if (!token) {
       return res.status(401).json({
         success: false,
