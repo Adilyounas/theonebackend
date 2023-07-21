003dc6245e9096c43d3d1f38b35d93cc6273cceb
Muhammad Adil <addhtml00@gmail.com>
2023-07-21 10:05:10 +0500

    asdf

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,15 +1,11 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel")
-const cookie = require('cookie');
+
 // *{<-------------IsAuthenticated / login or not---------->}*
 
 const isAuthenticated = async (req, res, next) => {
   try {
-    const cookies = cookie.parse(req.headers.cookie || '');
-    console.log("token");
-
-    const token = cookies.token;
-    console.log(token);
+    const { token } =  req.cookies;
     if (!token) {
       return res.status(401).json({
         success: false,
