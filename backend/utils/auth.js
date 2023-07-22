16d246a810bdcb7f2b3d4bf5dc69b2f4e73e56c7
Muhammad Adil <addhtml00@gmail.com>
2023-07-22 12:28:04 +0500

    const token = req.cookies['token'];

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -4,8 +4,9 @@ const User = require("../models/userModel")
 
 const isAuthenticated = async (req, res, next) => {
   try {
+
+    const token = req.cookies['token'];
     
-    const { token } =  req.cookies;
     if (!token) {
       return res.status(401).json({
         success: false,
