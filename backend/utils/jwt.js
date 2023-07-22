a37bd0bdcdc2f988be3b8f95e77bc28f293e8966
Muhammad Adil <addhtml00@gmail.com>
2023-07-22 18:04:11 +0500

    asdf

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -7,16 +7,15 @@ const sendJWT = async (user, res, statusCode, message) => {
     });
   }
 
-  // const options = {
-  //   expires: new Date(
-  //     Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-  //   ),
-  //   httpOnly: true,
-  //   secure: true,
-  //   sameSite: 'Lax',
-  // };
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: true,
+  };
 
-  res.status(statusCode).json({
+  res.status(statusCode).cookie("token",token,options) .json({
     success: true,
     message,
    token
